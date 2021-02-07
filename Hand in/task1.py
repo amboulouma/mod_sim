061f60da71ae9be99b0d1b68fb506c96cbbe58c7
Alexander Sing <AlexanderSing@live.de>
2021-02-07 13:05:32 +0100

    Formatting

--- Hand in/task1.py
+++ Hand in/task1.py
@@ -1,7 +1,6 @@
 from ForestDDEM import ForestDDEM
 
 if __name__ == '__main__':
-
     forest_ddem = ForestDDEM(deer_init=4000, fox_init=1000, t=120)
 
     forest_ddem.run_full(alpha=1e-5, beta=2e-5, gamma=5e-9, delta=5e-6, m=20000)
