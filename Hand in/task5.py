51495bb6ec7821d3ae03e652b26485e75474a074
Alexander Sing <AlexanderSing@live.de>
2021-02-08 15:19:26 +0100

    Formatting

--- Hand in/task5.py
+++ Hand in/task5.py
@@ -19,7 +19,6 @@ if __name__ == '__main__':
 
     forest_ddem = ForestDDEM(4000, 1000, 20)
 
-
     # Fit to deers
     bounds = ((0, 0, 0, 0, 5000), (5 * 10 ** -7, 10 ** -4, 10 ** -6, 10 ** -4, 10 ** 6))
 
@@ -28,25 +27,20 @@ if __name__ == '__main__':
     forest_ca.plot(show_plot=False)
     forest_ddem.plot(title="DDEM fitted to deers", new_window=False)
 
-    
     # Fit to foxes
     bounds = ((0, 0, 0, 0, 5000), (10 ** -7, 10 ** -4, 10 ** -6, 10 ** -4, 10 ** 6))
-    
+
     forest_ddem.fit_to_foxes(foxes=foxes, bounds=bounds)
 
     forest_ca.plot(show_plot=False)
     forest_ddem.plot(title="DDEM fitted to foxes", new_window=False)
-    
-
-
 
     alpha_fit, beta_fit, gamma_fit, delta_fit, M_fit = (
-         1.814528560849192e-07,
-         4.976322139740308e-05,
-         5e-07,
-         3.6060415775325e-05,
-         502500.0)
-
+        1.814528560849192e-07,
+        4.976322139740308e-05,
+        5e-07,
+        3.6060415775325e-05,
+        502500.0)
 
     forest_ddem.run_full(alpha=alpha_fit, beta=beta_fit, gamma=gamma_fit, delta=delta_fit, m=M_fit)
 
